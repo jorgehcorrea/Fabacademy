95cafab908caa8c61714f0514d32ff67d00aab75
jorge correa <info@jhc.design>
2019-05-12 23:57:14 -0500

    w2 update

---
+++ docs/images/w16_Applications/sketch_Bluetooth_stuff/sketch_Bluetooth_stuff.ino
@@ -0,0 +1,41 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BT(10, 11); // RX, TX  BLUE
+
+char dato;
+void setup() 
+{
+  BT.begin(38400); //VELOCIDAD DE COMUNICACION
+  Serial.begin(9600); 
+  pinMode(13,OUTPUT);
+  digitalWrite(13,LOW);
+  delay(5000);
+  BT.println("AT");
+  delay(1000);
+  BT.println("AT+ROLE=0");
+  delay(1000);
+  BT.println("AT+UART=9600");
+  delay(1000);
+  BT.println("AT+RESET");
+  delay(1000);
+  BT.begin(8600); 
+  
+}
+
+void loop() 
+{
+  if (BT.available())
+  {
+    dato=BT.read();
+    Serial.print(dato);
+  }
+  
+  if(dato=='1')
+  {
+    digitalWrite(13,HIGH);
+  }
+
+  if(dato=='0')
+  {
+    digitalWrite(13,LOW);
+  }
+}
